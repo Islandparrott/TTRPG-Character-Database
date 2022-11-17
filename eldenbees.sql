ab3812698e7b3278b2f55512d5734282bff7f485
Islandparrott <93695101+Islandparrott@users.noreply.github.com>
2022-11-17 14:53:39 -0800

    Update eldenbees.sql

--- eldenbees.sql
+++ eldenbees.sql
@@ -4,7 +4,7 @@ CREATE TABLE `Account` (
   `Username` varchar(100) NOT NULL,
   `Password` varchar(100) NOT NULL,
   PRIMARY KEY (`Username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) 
 
 DROP TABLE IF EXISTS `Equipment`;
 CREATE TABLE `Equipment` (
@@ -12,16 +12,14 @@ CREATE TABLE `Equipment` (
   `Description` varchar(550) DEFAULT NULL,
   `EID` int NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`Eqpmnt_Name`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+) 
 
 DROP TABLE IF EXISTS `EquipmentRepo`;
 CREATE TABLE `EquipmentRepo` (
-  `ERID`, int(11) NOT NULL,
+  `ERID`, int(11) AUTO_INCREMENT NOT NULL,
   `EID`, int(11) NOT NULL,
   PRIMARY KEY (`ERID`),
-  KEY `ERID` (`ERID`),
   KEY `EID` (`EID`),
-  CONSTRAINT `Class_ibfk_1` FOREIGN KEY (`ERID`) REFERENCES `Equipment` (`ERID`),
-  CONSTRAINT `Class_ibfk_2` FOREIGN KEY (`EID`) REFERENCES `Equipment` (`EID`),
+  CONSTRAINT `Class_ibfk_1` FOREIGN KEY (`EID`) REFERENCES `Equipment` (`EID`),
+) 
 
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;

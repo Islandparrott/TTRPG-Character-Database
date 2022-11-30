2d6b01e142cf74f821f4a74113f8c00b8e1a14a9
Islandparrott <93695101+Islandparrott@users.noreply.github.com>
2022-11-30 13:56:00 -0800

    Update and rename Spells.sql to inserts/strong/Spells.sql

--- inserts/strong/Spells.sql
+++ inserts/strong/Spells.sql
@@ -1,4 +1,4 @@
 
-insert into Spells (Spell_Name, Level, School, Spell_Range, Components, Duration, Attack, Damage, Description) 
+insert into Spells (Spell_Name, Level, School, Spell_Range, Components, Duration, Attack, Damage, Description)
     values ("Acid Splash", 0, "Conjuration", "60", "VS", "Instant", "DEX Save", "1d6 Acid", "Choose one or two creatures within 5 feet of eachother you can see in range.");
 
